96aa3196695b260052af030eddbd807968e5fc59
ppj <prasanna_3003@yahoo.com>
2014-12-03 11:59:25 +1100

    simplified tests for GitWrapper#connect and connect should now return the github obj

--- lib/git_wrapper.rb
+++ lib/git_wrapper.rb
@@ -4,12 +4,11 @@ class GitWrapper
   def connect
     raise ArgumentError, "Env Var not set" if env_var_not_set?
     github = Github.new oauth_token: ENV['OAUTH_TOKEN']
-    true
   end
 
   private
 
   def env_var_not_set?
-    ENV['OAUTH_TOKEN']==nil || ENV['OAUTH_TOKEN'].empty?
+    ENV['OAUTH_TOKEN']==nil || ENV['OAUTH_TOKEN'].strip.empty?
   end
 end
--- spec/lib/git_wrapper_spec.rb
+++ spec/lib/git_wrapper_spec.rb
@@ -6,13 +6,9 @@ describe GitWrapper do
     let(:github) {double('github')}
     let(:url) {'http://some_url'}
 
-    before do
-      allow(Github).to receive(:new).with(oauth_token: ENV['OAUTH_TOKEN'])
-    end
-
     it 'connects to github when the oauth token is given in env var' do
       ENV['OAUTH_TOKEN']='some sample env token'
-      expect(Github).to receive(:new).with(oauth_token: ENV['OAUTH_TOKEN'])
+      expect(Github).to receive(:new).with(oauth_token: ENV['OAUTH_TOKEN']).and_return(github)
       expect(subject.connect).to be_truthy
     end
 
@@ -22,7 +18,7 @@ describe GitWrapper do
     end
 
     it 'raises error and exits if the env variable for oauth token is set to empty' do
-      ENV['OAUTH_TOKEN']=''
+      ENV['OAUTH_TOKEN']='   '
       expect{subject.connect}.to raise_error(ArgumentError)
     end
   end
